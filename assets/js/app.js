b9f202367885c5130d1765183cc1647d51903aa1
14748 <sproplayerxd@gmail.com>
2023-10-29 12:46:19 +0100

    feat(OpenWeather): add class & methods to manage OpenWeather api requests
    This commit adds a class containing all the different endpoints we need and then three different methods that utilize this endpoints passing parameters

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,3 +17,52 @@ LocationIq.prototype.getListOfMatches = function (callback) {
         }
     })
 }
+
+function OpenWeather()
+{
+    this.currentWeather = "https://api.openweathermap.org/data/2.5/weather?appid=70183fe772720874c624c9f871eddbf8&units=metric&q="//PAMPLONA
+    this.currentWeatherLatLon = "https://api.openweathermap.org/data/2.5/weather?appid=70183fe772720874c624c9f871eddbf8&units=metric" //&lat=-2&lon=40
+    this.predictionWeather = "https://api.openweathermap.org/data/2.5/forecast?appid=70183fe772720874c624c9f871eddbf8&units=metric"
+}
+
+OpenWeather.prototype.getCurrentWeatherLatLon = function (lon, lat, callback){
+    $.ajax({
+        url: this.currentWeatherLatLon + "&lat=" + lat + "&lon="+ lon,
+        type: "GET",
+        dataType: "json",
+        success: function(data){
+            callback(data);
+        },
+        error: function(){
+            callback(null);
+        }
+    })
+}
+
+OpenWeather.prototype.getCurrentWeather = function (param, callback) {
+    $.ajax({
+        url: this.currentWeather + param,
+        type: "GET",
+        dataType: "json",
+        success: function(data){
+            callback(data);
+        },
+        error: function(){
+            callback(null);
+        }
+    })
+}
+
+OpenWeather.prototype.getPredictionWeather = function(lon, lat, callback) {
+    $.ajax({
+        url: this.predictionWeather+"&lon="+lon+"&lat="+lat,
+        type: "GET",
+        dataType: "json",
+        success: function(data){
+            callback(data);
+        },
+        error: function(callback){
+            callback(null);
+        }
+    })
+}
